f1e8750a34867cca894948c30385e18d70ec7ad8
Ariel Gnapie <nathanariel346@gmail.com>
2025-12-19 02:39:08 +0000

    maj back

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 const dataRoute = require('./routes/dataRoutes');
 const authRoute = require('./routes/authRoutes');
 const { getIo } = require('./socket');
+
 const app = express();
 
-//Autorisations cors
-//midleware general qui s'applique à tou tes les routes
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-  next();
-});
+app.use(cors({
+    origin: [
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'https://aquifer.casur-consulting.com' 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 app.use(express.json());
 
-//Injecter io dans les dans les requetes 
+// Injecter io
 app.use((req, res, next) => {
-  req.io = getIo();
-  next();
+    req.io = getIo();
+    next();
 });
 
 app.use('/api/data', dataRoute);
 app.use('/api/auth', authRoute);
-
-// setInterval(async () => {
-//     try{
-//         await donneesRegulieres.findOneAndUpdate(
-//             {},
-//             {
-//               volumeHeure,
-//               volumeMensuel,
-//               volumeTotal,
-//               compteurSecondes,
-//               moisActuel
-//             },
-//             { upsert: true }
-//         );
-//         console.log('État sauvegardé dans MongoDB');
-//     }catch (err) {
-//         console.error('Erreur de sauvegarde de l’état :', err.message);
-//     }
-// }, 60_000); 
 
 module.exports = app;
